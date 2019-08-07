bb5dcbdf9d76ffd049ff5c5c1cda86a2228650d2
maldini3 <kulbitsky.vn@gmail.com>
2019-08-07 15:34:07 +0300

    Домашнее задание 4 урок

--- Classes/index.php
+++ Classes/index.php
@@ -15,12 +15,60 @@ class User
     }
 
     public function getInfo() {
-        return "{$this->name}"."{$this->login}"."{$this->password}";
+        $information =  "{$this->name}"."{$this->login}"."{$this->password}";
+        return $information;
     }
 
+    public function Test() {
+        echo 'test';
+    }
 
 }
 
 
-$user1 = new User("Uladzimir", "popopo", "123456");
+$user1 = new User("Uladzimir", "maldini3", "123456");
 echo $user1->getInfo();
+
+class Moderator extends User {
+    public $info;
+    public $rights;
+
+    function Hello(){
+        echo "Moderator is here";
+    }
+
+    function __construct($name, $login, $password, $info, $rights)
+    {
+        parent::__construct($name, $login, $password);
+        $this->info = $info;
+        $this->rights = $rights;
+    }
+
+    public function getInfo() {
+        $information =  parent::getInfo();
+        $information .="{$this->info}"."{$this->rights}";
+        return $information;
+    }
+}
+
+$moder = new Moderator('Admin', 'Admin', '123', "moderator", "true");
+echo '<br>';
+echo '<br>'.$moder->Hello();
+echo $moder->getInfo();
+
+
+//class Test
+//{
+//    private static $name;
+//
+//    public static function setName($name1) {
+//        self::$name = $name1;
+//    }
+//
+//    public static function getName(){
+//        return self::$name;
+//    }
+//}
+
+//Test::setName('Vovka');
+//echo Test::getName();
